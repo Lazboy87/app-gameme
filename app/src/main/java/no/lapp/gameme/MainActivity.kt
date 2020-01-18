cbb2e66c9c6a400baf9c053b65e21c924d4799b6
Lazboy87 <lassehovden@gmail.com>
2020-01-18 16:35:03 +0100

    added var and vars to login page

--- app/src/main/java/no/lapp/gameme/MainActivity.kt
+++ app/src/main/java/no/lapp/gameme/MainActivity.kt
@@ -2,11 +2,24 @@ package no.lapp.gameme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var userinp = findViewById<TextView>(R.id.usernameinp)
+        var passwordinp= findViewById<TextView>(R.id.passwordinp)
+        val loginbtn= findViewById<Button>(R.id.loginbtn)
+
+        loginbtn.setOnClickListener{}
+
+
+
+
+
     }
 }
