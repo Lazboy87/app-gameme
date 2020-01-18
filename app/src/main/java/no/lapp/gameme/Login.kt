b46330aab5277b578450defabb15b391ef9bddfd
Lazboy87 <lassehovden@gmail.com>
2020-01-18 20:56:39 +0100

    refactor names of xml

--- app/src/main/java/no/lapp/gameme/Login.kt
+++ app/src/main/java/no/lapp/gameme/Login.kt
@@ -10,7 +10,7 @@ class Login : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
 
         var userinp = findViewById<TextView>(R.id.usernameinp)
         var passwordinp= findViewById<TextView>(R.id.passwordinp)
