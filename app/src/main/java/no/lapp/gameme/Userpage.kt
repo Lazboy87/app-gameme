b6e8d1126f813f19c7b7c19b369c356226a1c543
LASSE <lassehovden@gmail.com>
2020-02-21 03:06:03 +0100

    added showing user info on page

--- app/src/main/java/no/lapp/gameme/Userpage.kt
+++ app/src/main/java/no/lapp/gameme/Userpage.kt
@@ -2,11 +2,39 @@ package no.lapp.gameme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class Userpage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.userpage)
+
+
+        var emailshow = findViewById<TextView>(R.id.userEmail)
+
+
+
+
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+            emailshow.setText(email)
+        }
+
+
+
     }
 }
