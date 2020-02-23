07185849f6cc15339b75eb33a35a9b524dcae97f
Lazboy87 <lassehovden@gmail.com>
2020-02-23 04:06:27 +0100

    added working data extraction from firbase db

--- app/src/main/java/no/lapp/gameme/Userpage.kt
+++ app/src/main/java/no/lapp/gameme/Userpage.kt
@@ -2,8 +2,11 @@ package no.lapp.gameme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.constraintlayout.solver.widgets.Snapshot
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class Userpage : AppCompatActivity() {
 
@@ -11,30 +14,61 @@ class Userpage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.userpage)
 
+        val TAG = "ClassName"
+
 
         var emailshow = findViewById<TextView>(R.id.userEmail)
+        var nameshow = findViewById<TextView>(R.id.userName)
+        var ageshow = findViewById<TextView>(R.id.userAge)
+        var nickshow = findViewById<TextView>(R.id.userNick)
+
 
 
 
 
         val user = FirebaseAuth.getInstance().currentUser
         user?.let {
-            // Name, email address, and profile photo Url
-            val name = user.displayName
+
             val email = user.email
-            val photoUrl = user.photoUrl
+            val userref = FirebaseDatabase.getInstance().getReference("Users").orderByChild("email").equalTo(email)
 
-            // Check if user's email is verified
-            val emailVerified = user.isEmailVerified
 
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
-            val uid = user.uid
-            emailshow.setText(email)
+
+            var listUSER= userref.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (postSnapshot in dataSnapshot.children) {
+                        var values = postSnapshot
+                        var name = values.child("name").value.toString().trim()
+                        var age = values.child("age").value.toString().trim()
+                        var nickname = values.child("nickname").value.toString().trim()
+
+                        nameshow.setText("Name:  "+name )
+                        emailshow.setText("Email  :"+email)
+                        ageshow.setText("Age  :"+age)
+                        nickshow.setText("Nickname:  "+nickname)
+
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Getting Post failed, log a message
+                    Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+                    // ...
+                }
+            })
+
+
+
+
+
+
         }
 
 
 
     }
 }
+
+
+
+
